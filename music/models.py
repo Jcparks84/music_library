6c3127c53567a3f6fca87d372defd435e036ecbb
John Parks <jcparks1984@gmail.com>
2022-03-10 14:48:28 -0500

    Created Music Model For Music App

--- music/models.py
+++ music/models.py
@@ -1,3 +1,12 @@
+from platform import release
 from django.db import models
 
 # Create your models here.
+class Music(models.Model):
+    title = models.CharField(max_length=255)
+    Artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.IntegerField()
+    genre = models.CharField(max_length=255)
+
+    
