c4a532f8d2f63a2ca3e365b1dee6313ead24c995
John Parks <jcparks1984@gmail.com>
2022-03-14 14:16:21 -0400

    Modified Like

--- music/models.py
+++ music/models.py
@@ -7,8 +7,8 @@ class Music(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
-    release_date = models.IntegerField()
+    release_date = models.DateField()
     genre = models.CharField(max_length=255)
-    liked = models.CharField(max_length=255)
+    liked = models.IntegerField(default=0)
 
 
